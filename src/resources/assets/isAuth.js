ec6c861b4d8a701fad0e1228b5b4a33e3ec5dffa
Emre Dipi <mail@emredipi.com>
2019-11-07 00:15:50 +0300

    missed isAuth.js in previous commit

--- src/resources/assets/isAuth.js
+++ src/resources/assets/isAuth.js
@@ -1,90 +1,96 @@
-let isAuth = function(user, newOptions = {}) {
+function defineIsAuth(user, newOptions = {}){
     let options = {
         texts: {
-            placeholder:"Type your password",
+            placeholder:"Type Your Password",
             wrong:"Wrong Password",
             error:"Error",
             button:"Login"
         },
-        username: 'email'
+        loginField: 'email'
     };
-
     $.extend(options, newOptions);
-
-    let posterror=()=>{
-        swal({
-            title:options.texts.error,
-            icon:"error"
-        });
-    };
-    function isAuthenticated(callback){
-        $.get("/isAuth").done(function (data) {
-            if(data.csrf!==csrf) update_csrf(data.csrf);
-            if (data.logged) {
-                if (callback) callback();
-            }else{
-                askPassword(callback);
-            }
-        }).fail(()=>posterror());
-    }
-    function askPassword(callback){
-        swal({
-            title: user.name,
-            icon:user.photo,
-            content: {
-                element: "input",
-                attributes: {
-                    placeholder: options.texts.placeholder,
-                    type: "password"
-                },
-            },
-            button: {
-                text: options.texts.button,
-                closeModal: false,
-            }
-        })
-            .then(password => {
-                if(password){
-                    postData = {
-                        password: password
-                    };
-                    postData[options.username] = user[options.username];
-
-                    $.post("/ajaxlogin",
-                        postData
-                    ).done(data=> {
-                        if(data.logged){
-                            swal.stopLoading();
-                            swal.close();
-                            if (callback) callback();
-                        }else{
-                            swal({
-                                title:options.texts.wrong,
-                                icon:"warning",
-                                timer: 1000,
-                            });
-                        }
-                    }).fail(()=>posterror());
+    let object={
+        posterror:function(){
+            swal({
+                title:options.texts.error,
+                icon:"error"
+            });
+        },
+        object:this,
+        csrf:null,
+        isAuth:function(callback){
+            $.get("/isAuth").done(function (data) {
+                if(data.csrf!==object.csrf) object.update_csrf(data.csrf);
+                if (data.logged) {
+                    if (callback) callback();
                 }else{
-                    swal.close();
+                    object.askPassword(callback);
+                }
+            }).fail(function () {
+                object.posterror();
+            });
+        },
+        askPassword:function(callback){
+            swal({
+                title: user.name,
+                icon:user.photo,
+                content: {
+                    element: "input",
+                    attributes: {
+                        placeholder: options.texts.placeholder,
+                        type: "password"
+                    },
+                },
+                button: {
+                    text: options.texts.button,
+                    closeModal: false,
+                }
+            })
+                .then(password => {
+                    if(password){
+                        $.post("/ajaxlogin",
+                            {
+                                username:user[options.loginField],
+                                password,
+                                loginField: options.loginField
+                            }
+                        ).done(data=> {
+                            if(data.logged){
+                                swal.stopLoading();
+                                swal.close();
+                                if (callback) callback();
+                            }else{
+                                swal({
+                                    title:options.texts.wrong,
+                                    icon:"warning",
+                                    timer: 1000,
+                                });
+                            }
+                        }).fail(function () {
+                            object.posterror();
+                        });
+                    }else{
+                        swal.close();
+                    }
+                });
+        },
+        update_csrf:function(newcsrf){
+            object.csrf=newcsrf;
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': newcsrf
                 }
             });
-    }
-    function update_csrf(newcsrf){
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': newcsrf
-            }
-        });
-        $("input[name='_token']").val(newcsrf);
-    }
-    let csrf=$('meta[name="csrf-token"]').attr('content');
-    update_csrf(csrf);
+            $("input[name='_token']").val(newcsrf);
+        }
+    };
+    object.update_csrf($('meta[name="csrf-token"]').attr('content'));
     $("form").submit(function (event) {
         let _this=this;
         event.preventDefault();
-        isAuthenticated(function () {
+        object.isAuth(function () {
             $(_this).unbind('submit').submit();
         });
     });
-};
+    return object;
+}
